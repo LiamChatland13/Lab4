1010325314e8faa1f8a28b391e98ed086224b5a6
liamc <liamc@LIAMSXPS15>
2023-10-30 17:52:37 -0400

    .

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,10 +1,27 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Lab4.Data;
+using Lab4.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Diagnostics;
 
 namespace Lab4.Controllers
 {
     public class HomeController : Controller
     {
         public IActionResult Index()
+        {
+            return View();
+
+        }
+
+        private readonly SportsDbContext _context;
+
+        public HomeController(SportsDbContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Error()
         {
             return View();
         }
--- Data/SportsDbContext.cs
+++ Data/SportsDbContext.cs
@@ -13,11 +13,14 @@ namespace Lab4.Data
         public DbSet<SportClub> SportClubs { get; set; }
         public DbSet<Fan> Fans { get; set; }
 
+        public DbSet<Subscription> subscriptions { get; set; }
+
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             modelBuilder.Entity<SportClub>().ToTable("SportClub");
             modelBuilder.Entity<Fan>().ToTable("Fan");
+            modelBuilder.Entity<Subscription>().ToTable("Subscription");
         }
 
     }
